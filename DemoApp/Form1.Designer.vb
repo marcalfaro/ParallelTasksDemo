50abcd0c84e2ec070cfc0e9a2fdbe45bd273c8a2
Marc <marcalfaro@live.com>
2019-11-02 16:10:06 +0800

    Add project files.

---
+++ DemoApp/Form1.Designer.vb
@@ -0,0 +1,88 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.b2 = New System.Windows.Forms.Button()
+        Me.b3 = New System.Windows.Forms.Button()
+        Me.b1 = New System.Windows.Forms.Button()
+        Me.b4 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'b2
+        '
+        Me.b2.Location = New System.Drawing.Point(55, 100)
+        Me.b2.Name = "b2"
+        Me.b2.Size = New System.Drawing.Size(221, 51)
+        Me.b2.TabIndex = 0
+        Me.b2.Text = "Execute All Tasks and Wait"
+        Me.b2.UseVisualStyleBackColor = True
+        '
+        'b3
+        '
+        Me.b3.Location = New System.Drawing.Point(55, 180)
+        Me.b3.Name = "b3"
+        Me.b3.Size = New System.Drawing.Size(221, 51)
+        Me.b3.TabIndex = 0
+        Me.b3.Text = "Execute Tasks by Batch and Wait"
+        Me.b3.UseVisualStyleBackColor = True
+        '
+        'b1
+        '
+        Me.b1.Location = New System.Drawing.Point(55, 26)
+        Me.b1.Name = "b1"
+        Me.b1.Size = New System.Drawing.Size(221, 51)
+        Me.b1.TabIndex = 0
+        Me.b1.Text = "Execute Task and Wait"
+        Me.b1.UseVisualStyleBackColor = True
+        '
+        'b4
+        '
+        Me.b4.Location = New System.Drawing.Point(55, 256)
+        Me.b4.Name = "b4"
+        Me.b4.Size = New System.Drawing.Size(221, 51)
+        Me.b4.TabIndex = 0
+        Me.b4.Text = "Execute Different Tasks and Wait"
+        Me.b4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(324, 349)
+        Me.Controls.Add(Me.b4)
+        Me.Controls.Add(Me.b3)
+        Me.Controls.Add(Me.b1)
+        Me.Controls.Add(Me.b2)
+        Me.DoubleBuffered = True
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents b2 As Button
+    Friend WithEvents b3 As Button
+    Friend WithEvents b1 As Button
+    Friend WithEvents b4 As Button
+End Class
---
+++ DemoApp/Form1.vb
@@ -0,0 +1,151 @@
+﻿Option Explicit On
+Imports System.Threading
+
+Public Class Form1
+
+#Region " Task related stuff "
+    Public myTasksNames As ArrayList = Nothing
+
+    Private Function getTaskNames() As ArrayList
+        Dim s As New ArrayList
+        For i As Integer = 1 To 20
+            s.Add($"Task #{i}")
+        Next
+        Return s
+    End Function
+
+    Private Async Function processTask(ByVal taskId As Integer) As Task(Of Integer)
+        'await a function with no internal await
+        Return Await Task.Run(Of Integer)(Function()
+                                              Console.WriteLine($"Executing { myTasksNames(taskId) }. Sleeping now.")
+                                              Threading.Thread.Sleep(5000)
+
+                                              Console.WriteLine($"{ myTasksNames(taskId) } function done.")
+                                              Return taskId
+                                          End Function)
+    End Function
+
+    Private Async Function sumFunction() As Task(Of Integer)
+        Console.WriteLine("sumFunction() > start")
+
+        Dim rslt As Integer = 0
+        For i As Integer = 1 To 10
+            rslt += i
+            Await Task.Delay(1000)
+        Next
+
+        Console.WriteLine("sumFunction() > done")
+        Return rslt
+    End Function
+
+    Private Async Function wordFunction() As Task(Of String)
+        Console.WriteLine("wordFunction() > start")
+
+        Dim arrMonths As New ArrayList
+
+        For i As Integer = 1 To 12
+            arrMonths.Add(MonthName(i))
+            Await Task.Delay(2000)
+        Next
+
+        Console.WriteLine("wordFunction() > done")
+        Return String.Join(",", arrMonths.ToArray)
+    End Function
+#End Region
+
+
+    Private Async Sub b1_Click(sender As Object, e As EventArgs) Handles b1.Click
+        Dim txt As String = b1.Text : b1.Text = "Running"
+
+        'initialize tasks
+        myTasksNames = getTaskNames()
+
+        'execute 1 task only (at index 0) and wait
+        Console.WriteLine("Waiting for task to complete...")
+        Dim rslt As Integer = Await processTask(0)
+
+        'inform that you're done; return the result if necessary
+        Console.WriteLine($"Task { rslt } have been executed.")
+        b1.Text = txt
+    End Sub
+
+    Private Async Sub b2_Click(sender As Object, e As EventArgs) Handles b2.Click
+        Dim txt As String = b2.Text : b2.Text = "Running"
+
+        'initialize tasks
+        myTasksNames = getTaskNames()
+
+        'create a list of all your tasks to execute
+        Dim taskList As New List(Of Task)
+
+        'start all tasks and put them on your list
+        For i As Integer = 0 To (myTasksNames.Count - 1)
+            Dim newTask As Task = processTask(i)
+            taskList.Add(newTask)
+        Next
+
+        'wait for all tasks to finish before proceeding
+        Console.WriteLine("Waiting for all tasks to complete...")
+        Await Task.WhenAll(taskList.ToArray)
+
+        Console.WriteLine("All Tasks have been executed.")
+        b2.Text = txt
+    End Sub
+
+    Private Async Sub btn3_Click(sender As Object, e As EventArgs) Handles b3.Click
+        Dim txt As String = b3.Text
+        b3.Text = "Running"
+
+        'initialize tasks
+        myTasksNames = getTaskNames()
+
+        'create a list of all your tasks to execute
+        Dim taskList As New List(Of Task)
+
+        'run 5 tasks at a time
+        Dim maxConcurrent As Integer = 5
+
+        'start all tasks and put them on your list
+        For i As Integer = 0 To (myTasksNames.Count - 1)
+            Dim newTask As Task = processTask(i)
+            taskList.Add(newTask)
+
+            'throttle your tasks to a defined limit
+            If taskList.Count > maxConcurrent Then
+                Console.WriteLine("Limit reached. Stalling until a task is done....")
+                Dim completedTask As Task(Of Integer) = Await Task.WhenAny(taskList)
+                taskList.Remove(completedTask)
+            End If
+        Next
+
+        Console.WriteLine("Waiting for the rest of the tasks to complete...")
+        Await Task.WhenAll(taskList)
+
+
+        Console.WriteLine("All Tasks have been executed.")
+        b3.Text = txt
+    End Sub
+
+
+
+    Private Async Sub b4_Click(sender As Object, e As EventArgs) Handles b4.Click
+        Dim txt As String = b4.Text : b4.Text = "Running"
+
+        'start the sumFunction
+        Dim sumTask = sumFunction()
+
+        'start the wordFunction
+        Dim wordTask = wordFunction()
+
+        'wait for both tasks to finish before proceeding
+        Console.WriteLine("Waiting for all tasks to complete...")
+        Await Task.WhenAll(sumTask, wordTask)
+
+        'tasks finished. display results
+        Console.WriteLine("")
+        Console.WriteLine($"Result of sumTask() is: { sumTask.Result }")
+        Console.WriteLine($"Result of wordFunction() is: { wordTask.Result }")
+        b4.Text = txt
+    End Sub
+
+End Class
